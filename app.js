db1e07465536cf6bf323f5740b4dd957e71d8aa5
audace-kwizera <companyfreshbeauty@gmail.com>
2022-04-12 00:28:49 +0200

    exemple routes

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ const express = require('express');
 // Création de l'application express
 const app = express();
 
+// Donner accès au corps de la requête
+app.use(express.json());
+
 // Autorisation cors
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
+});
+
+app.post('api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'objet crée !'
+  });
 });
 
 app.use('/api/stuff', (req, res, next) => {
