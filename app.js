417a139af9db7819e9367dd0f851f89c7dd813ec
audace-kwizera <companyfreshbeauty@gmail.com>
2022-04-11 23:42:23 +0200

    Amélioration du serveur express

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ const express = require('express');
 // Création de l'application express
 const app = express();
 
-app.use((req, res) =>{
+app.use((req, res, next) => {
+    console.log('requete reçu');
+    // terminer requête et renvoyer vers prochain middleware
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) =>{
     res.json({ message: 'votre requête a bien été reçu' });
-})
+    next();
+});
+
+app.use((req, res) => {
+    console.log('Réponse nevoyé avec succès');
+});
 
 // Exportatin de l'application express
 module.exports = app;
--- server.js
+++ server.js
@@ -2,8 +2,49 @@ const http = require('http');
 // import de l'application
 const app = require('./app');
 
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
 // installation du port où l'application va tourner
-app.set('port', process.env.PORT || 3000);
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 3000);
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
+});
+
+server.listen(port);
