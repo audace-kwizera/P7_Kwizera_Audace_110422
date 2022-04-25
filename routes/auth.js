e4566110357a9eb6a370353fcae88fb213ddd1e7
audace-kwizera <companyfreshbeauty@gmail.com>
2022-04-25 22:44:51 +0200

    fix login db

---
+++ routes/auth.js
@@ -0,0 +1,18 @@
+// Login
+module.exports = function ({ app, dbConn }) {
+  app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    if (email && password) {
+      const sql = "SELECT * FROM user WHERE email = ? AND password = ?";
+      dbConn.query(sql, [email, password], function (error, response) {
+        if (response && response.length !== 0) {
+          res.status(200).jsonp({ ...response[0] });
+        } else {
+          res.status(200).jsonp({ message: "Your username or password is not correct" });
+        }
+      });
+    } else {
+      res.status(200).jsonp({ message: "Your username or password is not correct" });
+    }
+  });
+};
