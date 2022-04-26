71fe48b63827d242f5252c4c965168e9d6f98e1a
audace-kwizera <companyfreshbeauty@gmail.com>
2022-04-26 23:34:15 +0200

    fix notification et reactions routes crées.

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,5 +1,32 @@
-
-
+// Les notifications
 module.exports = function ({ app, dbConn, upload }) {
+  // Création d'une notification
+  app.post('/notifications/create', (req, res) => {
+    const { notificationImage, notificationMessage, userId } = req.body;
+    if (!notificationImage || !notificationMessage || !userId) {
+      res.status(200).jsonp({ message: 'Oups il y a une petit problème, veuillez réessayer' });
+    }
+    const createNotification = [[notificationImage, notificationMessage, userId]];
+    const createNotificationSql = "INSERT INTO user_notification (notification_image, notification_message, user_id) VALUES ?";
+    dbConn.query(createNotificationSql, [createNotification], function (error, insertedNotification) {
+      if (insertedNotification) {
+        res.status(200).jsonp({ id: insertedNotification.insertId, notification_image: notificationImage, notification_message: notificationMessage, user_id: userId });
+      } else {
+        res.status(200).jsonp({ message: 'Oups il y a une petit problème, veuillez réessayer' });
+      }
+    });
+  });
 
+  // Afficher les notifications en particulier par l'id de l'utilisateur
+  app.get('/notifications/:id', (req, res) => {
+    const userId = req.params.id;
+    const getNotificationsSql = "SELECT * FROM user_notification WHERE user_id = ? ORDER BY id DESC";
+    dbConn.query(getNotificationsSql, [userId], function (error, notifications) {
+      if (notifications) {
+        res.status(200).jsonp(notifications);
+      } else {
+        res.status(200).jsonp({ message: 'Oups il y a une petit problème, veuillez réessayer' });
+      }
+    });
+  });
 }
--- routes/reactions.js
+++ routes/reactions.js
@@ -1,5 +1,50 @@
-
-
+// Affichage des reactions
 module.exports = function ({ app, dbConn, upload }) {
+  app.post('/reactions/get', (req, res) => {
+    const { post_id, user_id } = req.body;
+    if (!post_id || !user_id) {
+      res.status(200).jsonp({ message: 'Oups il y a une petit problème, veuillez réessayer' });
+    }
+    const getReactionSql = "SELECT * FROM post_reaction WHERE post_id = ? AND user_id = ?";
+    dbConn.query(getReactionSql, [post_id, user_id], function (error, response) {
+      if (response && response.length) {
+        res.status(200).jsonp({ ...response[0] });
+      } else {
+        res.status(200).jsonp({ message: 'Oups il y a une petit problème, veuillez réessayer' });
+      }
+    });
+  });
 
-}
+  // Reaction sur un post
+  app.post('/reactions/create', (req, res) => {
+    const { postId, userId } = req.body;
+    if (!postId || !userId) {
+      res.status(200).jsonp({ message: 'Cannot create the post reaction, please try again' });
+    }
+    const reactions = [[postId, userId]];
+    const insertReactionSql = "INSERT INTO post_reaction (post_id, user_id) VALUES ?";
+    dbConn.query(insertReactionSql, [reactions], function (error, insertedReaction) {
+      if (insertedReaction) {
+        res.status(200).jsonp({ insertId: insertedReaction.insertId, post_id: postId, user_id: userId });
+      } else {
+        res.status(200).jsonp({ message: 'Cannot create the post reaction, please try again' });
+      }
+    });
+  });
+
+  // Suppression de la réaction
+  app.post('/reactions/delete', (req, res) => {
+    const { postId, userId } = req.body;
+    if (!postId || !userId) {
+      res.status(200).jsonp({ message: 'Oups il y a une petit problème, veuillez réessayer' });
+    }
+    const deleteReactionsSql  = "DELETE FROM post_reaction WHERE post_id = ? AND user_id = ?";
+    dbConn.query(deleteReactionsSql, [postId, userId], function (error, response) {
+      if (response && response.affectedRows) {
+        res.status(200).jsonp({ postId, userId });
+      } else {
+        res.status(200).jsonp({ message: 'Oups il y a une petit problème, veuillez réessayer' });
+      }
+    });
+  });
+};
